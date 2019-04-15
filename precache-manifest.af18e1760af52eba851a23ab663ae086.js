94a33092d801eb1c23c552ccbef486b9e68a54cc
Abie <abiesagitarius@gmail.com>
2019-04-15 11:06:30 +0700

    Updates

---
+++ precache-manifest.af18e1760af52eba851a23ab663ae086.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0fb0583175822a65d27",
+    "url": "/seasons-app/static/js/runtime~main.bbf91132.js"
+  },
+  {
+    "revision": "956f3c955c3bdf037b01",
+    "url": "/seasons-app/static/js/main.afc7e0b3.chunk.js"
+  },
+  {
+    "revision": "607320d00d64b68e18da",
+    "url": "/seasons-app/static/js/2.2050718e.chunk.js"
+  },
+  {
+    "revision": "b02ccffbd03eb8cc2a87a99316ff6756",
+    "url": "/seasons-app/index.html"
+  }
+];
